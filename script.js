798ffd03359a1c488fa97603d648c8a7840c5998
Nicola <nicoanta95@gmail.com>
2024-07-23 16:23:29 +0200

    hook javascript to DOM

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 const gameBoard = (function () {
-  const ticTacToeBoard = document.querySelector(".gameboard"); 
-  // this has to go to the playGame module
-  for (let i = 0; i < 9; i++) {
-    let square = document.createElement("div");
-    square.setAttribute("class", "square");
-    square.setAttribute("id", `${i}`);
-    // square.addEventListener("click", makeMove());
-    ticTacToeBoard.appendChild(square);
-  }
   let board = [
     "", "", "",
     "", "", "",
@@ -16,21 +7,22 @@ const gameBoard = (function () {
   
   const getBoard = () => board;
   
-  const makeMove = (e, player) => { // player is the player's sign
-    const boardIndex = e.getID;
-    board[boardIndex] = player; 
-  }
-  
-  const isMoveValid = (move) => {
-    const row = move[0];
-    const column = move[1];
-    if (board[row][column] === null) {
-      return true;
+  const handleMove = (e) => { // player is the player's sign
+    const boardIndex = Number(e.target.id);
+    const sign = gameLogic.getPlayer().sign;
+    board[boardIndex] = sign;
+    e.target.textContent = sign;
+    if (!gameLogic.isGameOver()){
+      if (gameLogic.winConditions()){
+        gameLogic.gameIsOver();
+        gameLogic.stopGame();
+      } else {
+        gameLogic.switchPlayer();
+      }
     }
-    return false;
+    e.target.removeEventListener("click", handleMove);
   }
-  
-  return { getBoard, getCoordinates, makeMove, isMoveValid };
+  return { getBoard, handleMove };
 })();
 
 function player(sign) {
@@ -38,10 +30,6 @@ function player(sign) {
 }
 
 const gameLogic = (function () {
-  let round = 1;
-  
-  const increaseRound = () => round++;
-  const getRound = () => round;
 
   const playerOne = player('X');
   const playerTwo = player('O');
@@ -54,67 +42,38 @@ const gameLogic = (function () {
   
   const isGameOver = () => gameOver;
   const gameIsOver = () => gameOver = true;
-
+  const stopGame = () => {
+    document.querySelectorAll(".square").forEach(square => {
+      square.removeEventListener("click", gameBoard.handleMove);
+    });
+  }
   // win conditions
   const winConditions = () => {
     const board = gameBoard.getBoard();
+    const winCombos = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],//rows
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],//columns
+      [0, 4, 8], [2, 4, 6]//diagonals
+    ]
 
-    const rowsWin = (row) => {
-      return board[row][0] !== null && board[row][0] === board[row][1] && board[row][0] === board[row][2];
-    };
-
-    const columnsWin = (column) => {
-      return board[0][column] !== null && board[0][column] === board[1][column] && board[0][column] === board[2][column];
-    };
-
-    const diagonalsWin = () => {
-      const firstDiagonal = [board[0][0], board[1][1], board[2][2]];
-      const secondDiagonal = [board[0][2], board[1][1], board[2][0]];
-      return (firstDiagonal[0] !== null && firstDiagonal[0] === firstDiagonal[1] && firstDiagonal[0] === firstDiagonal[2]) ||
-             (secondDiagonal[0] !== null && secondDiagonal[0] === secondDiagonal[1] && secondDiagonal[0] === secondDiagonal[2]);
-    };
-
-    return { rowsWin, columnsWin, diagonalsWin };
+    return winCombos.some(combo => {
+      const [a, b, c] = combo;
+      return board[a] !== "" && board[a] === board[b] && board[a] === board[c];
+    });
   };
   
-  return { increaseRound, getRound, switchPlayer, isGameOver, gameIsOver, getPlayer, winConditions };
+  return { switchPlayer, isGameOver, gameIsOver, stopGame, getPlayer, winConditions };
 })();
 
-/** const playGame = (function () {
-  while (!gameLogic.isGameOver()) {
-    const move = gameBoard.getCoordinates();
-    
-    if (gameBoard.isMoveValid(move)) {
-      const currentPlayer = gameLogic.getPlayer().sign;
-      gameBoard.makeMove(move, currentPlayer);
-      console.log(gameBoard.getBoard())
-      const { rowsWin, columnsWin, diagonalsWin } = gameLogic.winConditions();
-
-      for (let i = 0; i < 3; i++) {
-        if (rowsWin(i) || columnsWin(i)) {
-          console.log(`${currentPlayer} wins!`);
-          gameLogic.gameIsOver();
-          break;
-        }
-      }
-
-      if (diagonalsWin()) {
-        console.log(`${currentPlayer} wins!`);
-        gameLogic.gameIsOver();
-        break;
-      }
-
-      if (!gameLogic.isGameOver()) {
-        if (gameLogic.getRound() === 9) {
-          console.log("It's a tie!");
-          gameLogic.gameIsOver();
-          break;
-        }
-        gameLogic.increaseRound();
-        gameLogic.switchPlayer();
-      }
-    } else {
-      console.log("Invalid move, try again.");
-    }
+const playGame = (function () {
+  const ticTacToeBoard = document.querySelector(".gameboard"); 
+  const speaker = document.querySelector(".speaker");
+  speaker.textContent = "It's X's turn";
+  for (let i = 0; i < 9; i++) {
+    let square = document.createElement("div");
+    square.setAttribute("class", "square");
+    square.setAttribute("id", `${i}`);
+    ticTacToeBoard.appendChild(square);
+    square.addEventListener("click", gameBoard.handleMove);
   }
-})();**/
+})();
